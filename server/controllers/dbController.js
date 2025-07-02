07865243ab25947919b9d7eedb0e1c71ce04cf80
OfirSheffer <ofirshef@gmail.com>
2025-07-02 13:49:13 +0300

    server controller-final

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -1,24 +1,50 @@
 const Task = require('../models/Task');
-
 async function getTasks(req, res) {
-  // TODO
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
-
 async function addTask(req, res) {
-  // TODO
+    try {
+        const { title } = req.body;
+        const task = new Task({ title });
+        await task.save();
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
-
 async function toggleTask(req, res) {
-  // TODO
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+        task.done = !task.done;
+        await task.save();
+        res.json(task);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
-
 async function deleteTask(req, res) {
-  // TODO
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+        res.json({ message: 'Task deleted' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
 module.exports = {
-  getTasks,
-  addTask,
-  toggleTask,
-  deleteTask,
+    getTasks,
+    addTask,
+    toggleTask,
+    deleteTask,
 };
