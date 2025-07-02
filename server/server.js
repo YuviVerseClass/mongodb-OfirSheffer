b8276a862e8389a4d8a174b64b028cdcac4d9103
OfirSheffer <ofirshef@gmail.com>
2025-07-02 13:51:40 +0300

    server js-final

--- server/server.js
+++ server/server.js
@@ -2,16 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const apiRoutes = require('./routes/api');
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.get('/style/style.css', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../style/style.css'));
+});
+app.get('/js/script.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../js/script.js'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../index.html'));
+});
 app.use('/api', apiRoutes);
-
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB connection error:", err));
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
